2f2b10129f05c916d560c124cd0715104c18cf8d
Rey Thompson <rthompson1@gmail.com>
2015-05-19 14:22:26 -0500

    first commit

---
+++ mechanics.js
@@ -0,0 +1,17 @@
+// Challenge zero
+// 
+// The below function does not work. The variable
+// twoPlusTwo gets set to `undefined`. Refactor
+// the function to make it work.
+
+function addNumbers(numberA, numberB) {
+    console.log(numberA + numberB);
+}
+
+var twoPlusTwo = addNumbers(2, 2)
+
+return twoPlusTwo
+
+
+
+console.assert(twoPlusTwo === 4)
